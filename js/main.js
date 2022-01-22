e6a58c37d38c394cfb8a4c28b30daabf6dbe7602
Cenk Beri <cenkberi@gmail.com>
2022-01-22 19:46:54 +0300

    ok

--- js/main.js
+++ js/main.js
@@ -8,17 +8,16 @@ const likebox = document.getElementById('likebox');
 const conta = document.getElementById('conta');
 const maincont = document.getElementById('maincont');
 
-
 let repcount = 327;
 let retcount = 92;
 let likecount = 223;
 
-repbox.innerHTML = repcount;
-retbox.innerHTML = retcount;
+repbox.innerHTML  = repcount;
+retbox.innerHTML  = retcount;
 likebox.innerHTML = likecount;
 
-reply.addEventListener("click", function () {
-
+reply.addEventListener("click", function ()
+{
     reply.disabled = true;
     var newbox = document.createElement("div");
     newbox.className = "newbox bg-dark my-2";
@@ -30,13 +29,11 @@ reply.addEventListener("click", function () {
     username.placeholder = "@username...";
     newbox.appendChild(username);
 
-
     var userInput = document.createElement("input");
     userInput.type = "text";
     userInput.placeholder = "Write your tweet here...";
     userInput.className = "input-group bg-dark text-light userInput";
     newbox.appendChild(userInput);
-
 
     var sendbutton = document.createElement("button");
     sendbutton.className = "btn btn-info sendbutton";
@@ -44,80 +41,60 @@ reply.addEventListener("click", function () {
     sendbutton.style.float = "right";
     sendbutton.innerText = "Tweet!";
 
-
-     sendbutton.addEventListener("click", function () {
-
-        if (username.value != "" && userInput.value != "") {
-
+     sendbutton.addEventListener("click", function ()
+    {
+        if (username.value != "" && userInput.value != "")
+        {
             repcount++;
             repbox.innerHTML = repcount;
             newbox.style.display = "none";
             reply.disabled = false;
-
-
         }
 
-
     });
-     retweet.addEventListener("click", function () {
 
+     retweet.addEventListener("click", function ()
+     {
         newbox.innerHTML = "";
         reply.disabled = false;
-
     });
-     like.addEventListener("click", function () {
 
+     like.addEventListener("click", function ()
+    {
         newbox.innerHTML = "";
         reply.disabled = false;
-
     });
-
-
-
-
 });
 
-retweet.addEventListener("click", function () {
-
-
-    if (retcount <= 92) {
-
-
+retweet.addEventListener("click", function ()
+{
+    if (retcount <= 92)
+    {
         retcount++;
         retbox.innerHTML = retcount;
         retweet.className = "col-2 btn btn-secondary";
-
-
-    } else {
-
+    }
+    else
+    {
         retcount--;
         retbox.innerHTML = retcount;
         retweet.className = "col-2 btn btn-info";
-
-
     }
-
-
 });
 
 like.addEventListener("click", function () {
 
-    if (likecount <= 223) {
-
-
+    if (likecount <= 223)
+    {
         likecount++;
         likebox.innerHTML = likecount;
         like.className = "col-2 btn btn-secondary";
-
-
-    } else {
+    }
+    else
+    {
 
         likecount--;
         likebox.innerHTML = likecount;
         like.className = "col-2 btn btn-info";
-
-
     }
-
-
 });
